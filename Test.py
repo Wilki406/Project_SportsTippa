8b706494630a2dbe354e330c6206f7b267d3faaa
Benjamin <Killeroftime406@gmail.com>
2024-09-24 23:17:11 +1000

    buncha dogshit

--- Test.py
+++ Test.py
@@ -1,34 +1,51 @@
-import time
-from random import *
+from argon2 import PasswordHasher
 
-Yresponses = ["yes","y","ye","yea","yeah"]
+ph = PasswordHasher()
+hash = ph.hash("THIS IS MY PASSWORD")
+print(hash)
 
-money = int(input("How much money do you want to have! \n"))
+print(ph.verify(hash, "THIS IS MY PASSWORD"))
 
-def Gamble():
-    global money
-    if money == 0:
-        print("You are a disgrace")
-    print("meow :3")
-    gambleMoney = int(input("How much u want to gamble? \n"))
-    if gambleMoney > money:
-        print("You dont have that much")
-        Gamble()
 
-    print("are you ready!")
-    time.sleep(3)
-    Win = randint(0, 2)
-    print(Win)
-    if Win == 1:
-        winnings = gambleMoney * 2
-        print(f"You won {winnings}!")
-        money += winnings
-        print(f"Balance is now {money}")
-        Gamble()
-    else:
-        print(f"you Lost")
-        money -= gambleMoney
-        print(f"Balance is now {money}")
-        Gamble()
 
-Gamble()
+
+
+
+
+
+
+
+# import time
+# from random import *
+#
+# Yresponses = ["yes","y","ye","yea","yeah"]
+#
+# money = int(input("How much money do you want to have! \n"))
+#
+# def Gamble():
+#     global money
+#     if money == 0:
+#         print("You are a disgrace")
+#     print("meow :3")
+#     gambleMoney = int(input("How much u want to gamble? \n"))
+#     if gambleMoney > money:
+#         print("You dont have that much")
+#         Gamble()
+#
+#     print("are you ready!")
+#     time.sleep(3)
+#     Win = randint(0, 2)
+#     print(Win)
+#     if Win == 1:
+#         winnings = gambleMoney * 2
+#         print(f"You won {winnings}!")
+#         money += winnings
+#         print(f"Balance is now {money}")
+#         Gamble()
+#     else:
+#         print(f"you Lost")
+#         money -= gambleMoney
+#         print(f"Balance is now {money}")
+#         Gamble()
+#
+# Gamble()
--- Tippa.py
+++ Tippa.py
@@ -1,9 +1,11 @@
 import sqlite3
 import csv
 import os
+
 from customtkinter import *
 import customtkinter
 from screeninfo import get_monitors
+from argon2 import PasswordHasher
 
 conn = sqlite3.connect('database.db')
 cur = conn.cursor()
@@ -11,42 +13,47 @@ cur = conn.cursor()
 customtkinter.set_appearance_mode("dark")
 customtkinter.set_default_color_theme("dark-blue")
 
-
-
 class SignIn(customtkinter.CTk):
     def __init__(self):
         super().__init__()
         self.title("Tippa")
-        self.geometry("1600x800")
+        self.geometry("1140x570")
         self.resizable(width=False, height=False)
         self.current_page = None
 
-        self.mainframe = CTkFrame(self, corner_radius=10, fg_color="#292e2e")
+        self.mainframe = CTkFrame(self, fg_color="#292e2e", corner_radius=0)
         self.mainframe.grid(column=0, row=0, sticky='nsew')
 
         self.grid_rowconfigure((0, 1, 2), weight=1)
 
         self.mainframe.grid_columnconfigure(1, weight=1)
 
+        self.hiddenState = "*"
+
         self.initSigninPage()
         self.initRegisterPage()
+        self.initForgotPage()
 
         self.goSigninPage()
 
     def showFrame(self, frame):
-        for f in [self.loginframe, self.regframe]:
+        for f in [self.loginframe, self.regframe, self.forframe]:
             f.grid_remove()
         frame.grid(column=0,row=0, padx=(50), pady=(50), sticky='nsew')
 
+    def seePassword(self):
+        self.hiddenState = self.passcheck.get()
+        self.passwordentry.configure(show=self.hiddenState)
+
     def initSigninPage(self):
-        self.loginframe = CTkFrame(self, width=1560, height=760, corner_radius=10, fg_color="#404747")
+        self.loginframe = CTkFrame(self, width=1560, height=760, corner_radius=10, fg_color="#404747", border_color="#09d8eb", bg_color="#292e2e", border_width=2)
         self.loginframe.grid(column=0, row=0, padx=20, pady=20)
 
         self.titlelogin = CTkLabel(self.loginframe,
                                    text="Tippa",
                                    text_color="White",
                                    font=("Copperplate Gothic Bold",60))
-        self.titlelogin.grid(column=1, row=1, padx=400, pady=(10,10))
+        self.titlelogin.grid(column=1, row=1, padx=400, pady=(30,60))
 
         self.userentry = CTkEntry(self.loginframe,
                                placeholder_text="Username",
@@ -56,44 +63,103 @@ class SignIn(customtkinter.CTk):
                                corner_radius=10)
         self.userentry.grid(column=1, row=2, padx=400, pady=(10,10))
 
-        self.passwordentry = CTkEntry(self.loginframe,
+        self.passwordentry = CTkEntry(self.loginframe, show=self.hiddenState,
                                   placeholder_text="Password",
                                   width=240,
                                   height=36,
                                   border_width=2,
-                                  corner_radius=10)
-        self.passwordentry.grid(column=1, row=3, padx=400, pady=(10, 50))
+                                  corner_radius=10,)
 
-        self.passcheck = CTkCheckBox(self.loginframe, text="")
-        self.passcheck.grid(column=1,row=3,padx=(360,0),pady=(10,50))
+        self.passwordentry.grid(column=1, row=3, padx=400, pady=(10, 30))
 
-        self.loginbutton = CTkButton(self.loginframe, text='Login',)
-        self.loginbutton.grid(column=1, row=4, pady=(10), padx=(200,0))
+        self.passcheck = CTkCheckBox(self.loginframe, text="",command=self.seePassword,
+                               onvalue="", offvalue="*")
+        self.passcheck.grid(column=1,row=3,padx=(360,0),pady=(10,30))
+
+        self.SIerrormessage = CTkLabel(self.loginframe, text="", text_color="red")
+        self.SIerrormessage.grid(column=1, row=4)
+
+        self.loginbutton = CTkButton(self.loginframe, text='Login',
+                                 width=300,
+                                 border_width=0,
+                                 corner_radius=8,)
+        self.loginbutton.grid(column=1, row=5, pady=(10,30))
+
+        self.forgotbutton = CTkButton(self.loginframe, text='Forgot Password?', command=self.goForgotPage)
+        self.forgotbutton.grid(column=1, row=6, pady=(10,100), padx=(0,160))
 
         self.regbutton = CTkButton(self.loginframe, text='Register', command=self.goRegisterPage)
-        self.regbutton.grid(column=1, row=4, pady=(10), padx=(0,200))
-
-
+        self.regbutton.grid(column=1, row=6, pady=(10,100), padx=(160,0))
 
     def initRegisterPage(self):
-        self.regframe = CTkFrame(self, width=1560, height=760, corner_radius=10, fg_color="#404747")
+        self.regframe = CTkFrame(self, width=1560, height=760, corner_radius=10, fg_color="#404747",
+                                   border_color="#09d8eb", bg_color="#292e2e", border_width=2)
         self.regframe.grid(column=0, row=0, padx=20, pady=20)
 
-        self.titlereg = CTkLabel(self.regframe, text="Tippa",text_color="White", font=("Copperplate Gothic Bold",60))
-        self.titlereg.grid(column=1, row=1, padx=400, pady=(10,200))
+        self.titlereg = CTkLabel(self.regframe,
+                                   text="Tippa",
+                                   text_color="White",
+                                   font=("Copperplate Gothic Bold", 60))
+        self.titlereg.grid(column=1, row=1, padx=400, pady=(30, 60))
 
-        self.backbutton = CTkButton(self.regframe, text='Back', command=self.goSigninPage)
-        self.backbutton.grid(column=1, row=2)
+        self.nameentry = CTkEntry(self.regframe,
+                                  placeholder_text="Fullname",
+                                  width=240,
+                                  height=36,
+                                  border_width=2,
+                                  corner_radius=10)
+        self.nameentry.grid(column=1, row=2, padx=400, pady=(10, 10))
 
+        self.userentry = CTkEntry(self.regframe,
+                                  placeholder_text="Username",
+                                  width=240,
+                                  height=36,
+                                  border_width=2,
+                                  corner_radius=10)
+        self.userentry.grid(column=1, row=3, padx=400, pady=(10, 10))
+
+        self.passwordentry = CTkEntry(self.regframe, show=self.hiddenState,
+                                      placeholder_text="Password",
+                                      width=240,
+                                      height=36,
+                                      border_width=2,
+                                      corner_radius=10, )
+
+        self.passwordentry.grid(column=1, row=4, padx=400, pady=(10, 30))
+
+        self.backbuttonreg = CTkButton(self.regframe, text='Back', command=self.goSigninPage)
+        self.backbuttonreg.grid(column=1, row=6)
+
+    def initForgotPage(self):
+        self.forframe = CTkFrame(self, width=1560, height=760, corner_radius=10, fg_color="#404747",
+                                 border_color="#09d8eb", bg_color="#292e2e", border_width=2)
+        self.forframe.grid(column=0, row=0, padx=20, pady=20)
+
+        self.titlefor = CTkLabel(self.forframe,
+                                 text="Tippa",
+                                 text_color="White",
+                                 font=("Copperplate Gothic Bold", 60))
+        self.titlefor.grid(column=1, row=1, padx=400, pady=(30, 60))
+
+        self.backbuttonfor = CTkButton(self.forframe, text='Back', command=self.goSigninPage)
+        self.backbuttonfor.grid(column=1, row=2)
 
     def goSigninPage(self):
         self.showFrame(self.loginframe)
+        self.SIerrormessage.configure(text="")
+        return
 
     def goRegisterPage(self):
         self.showFrame(self.regframe)
         return
 
+    def goForgotPage(self):
+        if self.userentry.get() == "":
+            self.SIerrormessage.configure(text="Please enter Username")
+            return
+
+        self.showFrame(self.forframe)
+        return
 
 app = SignIn()
 app.mainloop()
-
